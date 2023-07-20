40776e20d6a65f5eab30ad97cd7b020b5b72a3b4
Kevin_Dietmeyer <kevindietmeyer@gmail.com>
2023-07-20 17:12:47 -0400

    updated and connected to api

--- functions/src/exerciseLibrary.js
+++ functions/src/exerciseLibrary.js
@@ -4,33 +4,34 @@ const coll = db.collection('exercises');
 const toArray = (collection) => collection.docs.map(doc => ({ id: doc.id, ...doc.data() }))
 
 //get(retrieve) all exercises
-export async function getAllExercises(req,res) {
+export async function getAllExercises(req, res) {
   try {
-      const allExercises = await coll.get();
-      res.send(toArray(allExercises));
+    const allExercises = await coll.get();
+    res.send(toArray(allExercises));
   } catch (err) {
-      res.status(500).send(err);
+    res.status(500).send(err);
   }
 }
 
 //add a new exercise 
-export async function addNewExercise(req,res) {
+export async function addNewExercise(req, res) {
   try {
-      const newExercise = req.body;
-      await coll.add(newExercise);
-      getAllExercises(req,res);
+    const newExercise = req.body
+    console.log(newExercise)
+    await coll.add(newExercise)
+    getAllExercises(req, res)
   } catch (err) {
-      res.status(500).send(err);
+    res.status(500).send(err);
   }
 }
 
-export async function updateExerciseById(req,res) {
+export async function updateExerciseById(req, res) {
   try {
-      const { exerciseId } = req.params
-      const updatedInfo = req.body;
-      await coll.doc(exerciseId).update(updatedInfo);
-    getAllExercises(req,res);
+    const { exerciseId } = req.params
+    const updatedInfo = req.body;
+    await coll.doc(exerciseId).update(updatedInfo);
+    getAllExercises(req, res);
   } catch (err) {
-      res.status(500).send(err);
+    res.status(500).send(err);
   }
 }
